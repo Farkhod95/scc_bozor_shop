896a26179aaf2eb73c8df70ae9748cddba347eac
Asqarxon <asqarxon77777@gmail.com>
2026-01-17 19:20:28 +0500

    Refactor translation logic in translations.py to simplify field value retrieval

--- src/apps/core/utils/translations.py
+++ src/apps/core/utils/translations.py
@@ -5,15 +5,16 @@ def translate_response(obj, fields: list[str], lang: str, is_admin: bool) -> dic
     data = {"id": obj.id}
 
     for field in fields:
+        translated_field = f"{field}_{lang}"
+        value = getattr(obj, translated_field, None) or getattr(obj, field, None)
+
         if is_admin:
-            data[field] = getattr(obj, field, None)
+            data[field] = value
             data[f"{field}_uz"] = getattr(obj, f"{field}_uz", None)
             data[f"{field}_ru"] = getattr(obj, f"{field}_ru", None)
             data[f"{field}_en"] = getattr(obj, f"{field}_en", None)
             data[f"{field}_uz_cyrl"] = getattr(obj, f"{field}_uz_cyrl", None)
         else:
-            translated_field = f"{field}_{lang}"
-            value = getattr(obj, translated_field, None) or getattr(obj, field, None)
             data[field] = value
     return data
 
