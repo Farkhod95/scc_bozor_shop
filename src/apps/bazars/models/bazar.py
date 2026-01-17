91d9ccbe44632884009e7a609bf3e4d074b4c731
Asqarxon <asqarxon77777@gmail.com>
2026-01-17 21:42:18 +0500

    Update CORS settings to specify allowed origins

--- src/apps/bazars/models/bazar.py
+++ src/apps/bazars/models/bazar.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Avg, Count
 
 from apps.core.models import BaseModel
 
@@ -11,5 +12,15 @@ class Bazar(BaseModel):
     lat = models.FloatField(null=True, blank=True)
     lng = models.FloatField(null=True, blank=True)
 
+    average_rating = models.FloatField(default=0.0)
+    review_count = models.IntegerField(default=0)
+
     def __str__(self):
         return self.name
+
+    def recalculate_rating(self):
+        stats = self.reviews.aggregate(avg=Avg('rating'), count=Count('id'))
+
+        self.average_rating = stats['avg'] or 0.0
+        self.review_count = stats['count'] or 0
+        self.save()
