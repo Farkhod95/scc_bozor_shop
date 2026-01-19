95708ec4975e41ac9883279728d0637c1ecf2f2e
Asqarxon <asqarxon77777@gmail.com>
2026-01-19 17:08:19 +0500

    Add role field to user serialization

--- src/apps/accounts/views/list_user.py
+++ src/apps/accounts/views/list_user.py
@@ -21,6 +21,7 @@ class ListUserSerializer(serializers.Serializer):
     last_name = serializers.CharField()
     email = serializers.EmailField()
     phone_number = serializers.CharField()
+    role = serializers.IntegerField()
     profile_image = serializers.CharField(allow_null=True)
 
 
